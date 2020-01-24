2f458a4fc7c257c67c183829ff0c1a4623cb5a05
Garrett Reichman <gsreichman@gmail.com>
2020-01-24 09:53:33 -0500

    updated location and episode card creation

--- assets/javascript/rm.js
+++ assets/javascript/rm.js
@@ -175,28 +175,39 @@ function createLocationElement(response) {
             people[i] = getLinkId(people[i]);
         }//for each person
 
+        var colDiv = $("<div>");
+        colDiv.addClass("col");
         var locDiv = $("<div>");
+        locDiv.addClass("card");
+        var cardImage = $("<div>");
+        cardImage.addClass("card-image");
         var locPic = $("<img>");
         locPic.attr("src", "https://i.guim.co.uk/img/media/b563ac5db4b4a4e1197c586bbca3edebca9173cd/0_12_3307_1985/master/3307.jpg?width=300&quality=85&auto=format&fit=max&s=a84d55a053bad561a57034beff1f1243");
         locPic.attr("alt", locData[0] + " picture");
-        locDiv.append(locPic);
-        var locName = $("<h4>");
+        cardImage.append(locPic);
+        var locName = $("<span>");
         locName.text(locData[0]);
-        locDiv.append(locName);
+        locName.addClass("card-title");
+        cardImage.append(locName);
+        locDiv.append(cardImage);
+        var cardContent = $("<div>");
+        cardContent.addClass("card-content");
         var locDimension = $("<p>");
         locDimension.text("Dimension: " + locData[1]);
-        locDiv.append(locDimension);
+        cardContent.append(locDimension);
         locPeople = $("<p>");
         locPeople.attr("data-people", JSON.stringify(people));
         locPeople.text("Click to view the " + people.length + " characters in this location.");
         locPeople.addClass("peopleLink")
-        locDiv.append(locPeople);
+        cardContent.append(locPeople);
+        locDiv.append(cardContent);
+        colDiv.append(locDiv);
         //console.log(response);
         //console.log(locData);
         console.log("Created: " +response.id+" "+ locData[0]);
 
 
-        return locDiv;
+        return colDiv;
     }//if not null
 
 }//createLocation
@@ -211,24 +222,35 @@ function createEpisodeElement(response) {
             people[i] = getLinkId(people[i]);
         }//for
 
+        var colDiv = $("<div>");
+        colDiv.addClass("col");
         var epDiv = $("<div>");
+        epDiv.addClass("card");
+        var cardImage = $("<div>");
+        cardImage.addClass("card-image");
         var epPic = $("<img>");
         epPic.attr("src", "https://i.guim.co.uk/img/media/b563ac5db4b4a4e1197c586bbca3edebca9173cd/0_12_3307_1985/master/3307.jpg?width=300&quality=85&auto=format&fit=max&s=a84d55a053bad561a57034beff1f1243");
         epPic.attr(epData[0] + " picture");
-        epDiv.append(epPic);
-        var epName = $("<h4>");
+        cardImage.append(epPic);
+        var epName = $("<span>");
+        epName.addClass("card-title");
         epName.text(epData[0]);
-        epDiv.append(epName);
+        cardImage.append(epName);
+        var cardContent = $("<div>");
+        cardContent.addClass("card-content");
         var epSeason = $("<p>");
         epSeason.text(epData[1]);
-        epDiv.append(epSeason);
+        cardContent.append(epSeason);
         var epChars = $("<p>");
         epChars.text("Click to view the " + people.length + " characters in this episode");
         epChars.attr("data-people", JSON.stringify(people));
         epChars.addClass("peopleLink")
-        epDiv.append(epChars);
+        cardContent.append(epChars);
+        epDiv.append(cardImage);
+        epDiv.append(cardContent);
+        colDiv.append(epDiv);
         console.log("Created: " + epData[0]);
-        return epDiv;
+        return colDiv;
     }//if not null
 
 }//create episode
